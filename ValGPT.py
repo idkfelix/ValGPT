06b4fef1bef73d2f8dd997acf7e89975e1f7f492
idkfelix <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>
2023-05-18 15:14:47 +1000

    stuff

--- ValGPT.py
+++ ValGPT.py
@@ -1,18 +1,25 @@
-import os
-import json
-import ssl
-import openai
-import asyncio
-import requests
-import websockets
-import threading
-from yaml import safe_load
+# Standard library imports
+from os import path, getenv, system
 from base64 import b64encode
+from json import loads as jsonLoads
+from ssl import SSLContext, PROTOCOL_TLS_CLIENT, CERT_NONE
+from asyncio import get_event_loop as async_get_event_loop
+from re import sub as resub
+
+# Third-party library imports
+from requests import request
+from websockets import connect, exceptions
+from openai import api_key, ChatCompletion
+from yaml import safe_load
 from urllib3 import disable_warnings, exceptions
+from threading import Thread
+
+
+# Disable unsecure request warnings and clear console
 disable_warnings(exceptions.InsecureRequestWarning)
+system('cls')
 
-os.system('cls')
-
+# Print banner
 print("""
 ____   ____      .__    _____________________________
 \   \ /   /____  |  |  /  _____/\______   \__    ___/
@@ -22,6 +29,7 @@ ____   ____      .__    _____________________________
                \/             \/                     
 """)
 
+# Define default prompt for config
 default_prompt = """you are a chat bot for a Valorant in game chat.
     Use mostly short sentences unless a longer response is expencted.
     Act as a normal person who responds unprofessionally to conversation.
@@ -29,7 +37,8 @@ default_prompt = """you are a chat bot for a Valorant in game chat.
     Speak casually with no punctuation, dont be appolagetic. \n\n 
     """
 
-if not os.path.exists("config.yml"):
+# If config dosent exist create it, else load it
+if not path.exists("config.yml"):
     default_config = f"openai_key: \nprompt: | \n    {default_prompt}"
     with open("config.yml", "w") as f:
                 f.write(default_config)
@@ -40,20 +49,23 @@ else:
     with open("config.yml", 'r') as f:
             config = safe_load(f)
 
+# Get values from Valorant lockfile
 def getlockData():
-    lockfilePath = os.path.join(os.getenv("LOCALAPPDATA")+"\Riot Games\Riot Client\Config\lockfile")
+    lockfilePath = path.join(getenv("LOCALAPPDATA")+"\Riot Games\Riot Client\Config\lockfile")
     with open(lockfilePath, 'r') as f:
         contents = f.read()
     d = {}
     d['name'], d['pid'], d['port'], d['password'], d['protocol'] = contents.strip().split(':')
     return d
 
+# Catch error if valorant isnt running
 try:
     lockfile = getlockData()
 except Exception:
      print("Make Sure Valorant Is running \n")
      input("Press Enter to continue...")
 
+# Get Player username
 def GetUsername():
     url = f"https://127.0.0.1:{lockfile['port']}/player-account/aliases/v1/active"
 
@@ -62,20 +74,20 @@ def GetUsername():
         "Authorization": f"Basic " + b64encode(('riot:' + lockfile['password']).encode()).decode()
     }
 
-    response = requests.request("GET", url, headers=headers, verify=False)
-    l = json.loads(response.text)
+    response = request("GET", url, headers=headers, verify=False)
+    l = jsonLoads(response.text)
     return l["game_name"]
 
+# Catch Valorant not running error
 try:
     username = GetUsername()
 except Exception:
      print("Make Sure Valorant Is running \n")
      input("Press Enter to continue...")
 
-
+# Define Valorant send message function
 def SendMessage(cid, msg):
     url = f"https://127.0.0.1:{lockfile['port']}/chat/v6/messages/"
-
     body = {
         "cid": cid,
         "message": msg,
@@ -86,11 +98,12 @@ def SendMessage(cid, msg):
         "Authorization": f"Basic " + b64encode(('riot:' + lockfile['password']).encode()).decode()
     }
 
-    requests.request("POST", url, json=body, headers=headers, verify=False)
+    request("POST", url, json=body, headers=headers, verify=False)
 
+# Define OpenAI completion function
 def GetResponse(messages):
-    openai.api_key = config["openai_key"]
-    completions = openai.ChatCompletion.create(
+    api_key = config["openai_key"]
+    completions = ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=messages
     )
@@ -102,20 +115,23 @@ def GetResponse(messages):
             continue
     return completions.choices[0].message.content
 
+# Define AI message function
 def aiMessage(cid, messages):
     GPTmsg = GetResponse(messages)
     SendMessage(cid,GPTmsg)
 
-ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+# Config for websocket
+ssl_context = SSLContext(PROTOCOL_TLS_CLIENT)
 ssl_context.check_hostname = False
-ssl_context.verify_mode = ssl.CERT_NONE
+ssl_context.verify_mode = CERT_NONE
 
 local_headers = {}
 local_headers['Authorization'] = 'Basic ' + b64encode(('riot:' + lockfile['password']).encode()).decode()
 url = f"wss://127.0.0.1:{lockfile['port']}"
 
+# Create websocket function
 async def ws():
-    async with websockets.connect(url, ssl=ssl_context, extra_headers=local_headers) as websocket:
+    async with connect(url, ssl=ssl_context, extra_headers=local_headers) as websocket:
         await websocket.send("[5, \"OnJsonApiEvent\"]")
 
         msgids = []
@@ -124,14 +140,17 @@ async def ws():
             {"role": "assistant", "name":username, "content": "whats up?"},
             ]
 
+        # Listen for messages from websocket
         while True:
             response = await websocket.recv()
 
-            if len(response) > 0 and json.loads(response)[2]['uri'] == "/chat/v6/messages":
-                cmsgid = json.loads(response)[2]["data"]["messages"][0]["id"]
-                msg = json.loads(response)[2]["data"]["messages"][0]
-                name = msg["game_name"]
-                body = msg["body"]
+            if len(response) > 0 and jsonLoads(response)[2]['uri'] == "/chat/v6/messages":
+                cmsgid = jsonLoads(response)[2]["data"]["messages"][0]["id"]
+                msg = jsonLoads(response)[2]["data"]["messages"][0]
+                unfixedName = msg["game_name"]
+                name = resub(r'[^\w\s~!@#\$%\^&\*_\-+=\|\\\(\){}\[\]:;"\'<>,\.\?/]+', ' ', unfixedName)
+                unfixedBody = msg["body"]
+                body = resub(r'[^\w\s~!@#\$%\^&\*_\-+=\|\\\(\){}\[\]:;"\'<>,\.\?/]+', ' ', unfixedBody)
                 cid = msg["cid"]
                 if cmsgid in msgids:
                     continue
@@ -141,13 +160,13 @@ async def ws():
 
                     if  name != username:
                         try:
-                            threading.Thread(target=aiMessage(cid, messages[-5:]), daemon=True).start()
+                            Thread(target=aiMessage(cid, messages[-5:]), daemon=True).start()
                         except Exception:
                             continue
                 msgids.append(cmsgid)
 
 try:
-    asyncio.get_event_loop().run_until_complete(ws())
-except websockets.exceptions.ConnectionClosedError:
+    async_get_event_loop().run_until_complete(ws())
+except exceptions.ConnectionClosedError:
      print("Make Sure Valorant Is running \n")
      input("Press Enter to continue...")
